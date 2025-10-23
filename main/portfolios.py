c7f582e7f5be30c3c85f9de0cc1994977ba78d12
Jacopo Binati <binatijacopo@gmail.com>
2025-10-23 17:34:37 +0200

    openpyxl

--- main/portfolios.py
+++ main/portfolios.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import altair as alt
+import openpyxl
 
 st.set_page_config(layout="wide")
 st.title("Galambos Capital Dashboard")
